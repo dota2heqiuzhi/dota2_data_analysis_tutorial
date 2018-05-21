bc4486fb7e1667601db3ad36a905c0c9b9503bdf
unknown <dota2heqiuzhi@gmail.com>
2018-05-21 21:55:17 +0800

    middle_tier_1_tower

---
+++ dota2_api.py
@@ -0,0 +1,13 @@
+import requests
+
+def get_api_json(url):
+    try:
+        r = requests.get(url, timeout=3)
+        r_json = r.json()
+        return r_json
+    except:
+        return get_api_json(url)
+
+if __name__ == '__main__':
+    # url = "https://www.niusport.net/GameAjax/BeforePlay"
+    pass
