63222309c846280fa2ce51e8ea9334acff8c7be9
dota2heqiuzhi <dota2heqiuzhi@gmail.com>
2021-09-10 17:17:12 +0800

    新增：第1、2课代码+数据源介绍

---
+++ 备份/.ipynb_checkpoints/dota2_api-checkpoint.py
@@ -0,0 +1,12 @@
+import requests
+
+def get_api_json(url):
+    try:
+        r = requests.get(url, timeout=3)
+        r_json = r.json()
+        return r_json
+    except:
+        return get_api_json(url)
+
+if __name__ == '__main__':
+#     pass
